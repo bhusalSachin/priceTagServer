5b9f0395863157a837ffab59f77cacc3902430c5
2bitCode <sachinb1377@gmail.com>
2022-01-19 23:49:28 +0545

    trick

--- models/db.js
+++ models/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 //connecting to mongodb using mongoose
-const url = process.env.MONGO_URI;
+const url =
+  " mongodb://admin:admin@pricedatabase-shard-00-00.re29j.mongodb.net:27017,pricedatabase-shard-00-01.re29j.mongodb.net:27017,pricedatabase-shard-00-02.re29j.mongodb.net:27017/priceDatabase?ssl=true&replicaSet=atlas-bew5c1-shard-0&authSource=admin&retryWrites=true&w=majority";
 mongoose
   .connect(url)
   .then((db) => {
